7ffa5a71e36e1a25143afab929e5b979e1c4f1a3
Mark Allyn <mark.a.allyn@intel.com>
2011-06-21 14:59:39 -0700

    Application: Watchdog Daemon: Introduction
    BZ: 3599
    
    Introduction of watchdog daemon used to monitor user space
    applications so that system will automatically reboot if
    a critical application terminates or is disabled
    
    Change-Id: I5f404c0547535806b8341f0023b2b7085916fa87
    Signed-off-by: Mark Allyn <mark.a.allyn@intel.com>

---
+++ watchdog_daemon/Android.mk
@@ -0,0 +1,30 @@
+# Copyright 2009 - 2010 (c) Intel Corporation. All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq ($(BUILD_WITH_WATCHDOG_DAEMON_SUPPORT),true)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES += hardware/intel/include
+LOCAL_CFLAGS += -g -Wall
+LOCAL_SRC_FILES:= watchdogd.c
+LOCAL_MODULE := watchdogd
+LOCAL_SHARED_LIBRARIES := libcutils libc
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
+endif
