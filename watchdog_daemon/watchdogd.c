3914eef07830f768cb82b48a2ce3f14de1c7072e
Mark Allyn <mark.a.allyn@intel.com>
2012-10-24 15:46:56 -0700

    [PORT FROM R4Enzo]Service: Watchdogd: Remove mlockall so that non root user can run watchdogd
    BZ: 99584
    
    This also moves watchdogd to the /system/bin directory because /sbin is
    not available to non root users
    
    Change output directory to root directory
    
    Change-Id: I6a9fe86d4d9a6136a0aba8d1ce3caf26ac3903eb
    ORIG-Change-Id: I1e7a09e0d79363a57af90e3da1cdb9e847e1de85
    Signed-off-by: Mark Allyn <mark.a.allyn@intel.com>
    Reviewed-on: http://android.intel.com:8080/101527
    Reviewed-by: Afantenos, Marie Cecile <marie.cecile.afantenos@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Jauffres, MichelX <michelx.jauffres@intel.com>
    Reviewed-by: Romieu, Benoit <benoit.romieu@intel.com>
    Tested-by: Molero Rodenas, BeatrizX <beatrizx.molero.rodenas@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- watchdog_daemon/watchdogd.c
+++ watchdog_daemon/watchdogd.c
@@ -64,11 +64,7 @@ int main(int argc, char **argv)
 	int n, ret, timeout = EXTTIMER_DEFAULT_TIMEOUT, wd_timeout;
 	int pre_timeout = WATCHDOG_TIME_BETWEEN_WARN_AND_RESET;
 	int options;
-	if ( (syscall(__NR_mlockall,MCL_CURRENT | MCL_FUTURE)) != 0) // syscall number 152 is mlockall
-	{
-		LOGE("Unable to lock all page\n");
-		goto error_nop;
-	}
+
 	/* Start logging here */
 	LOG_INIT(LOG_TAG);
 	LOGW("Watchdog daemon starting\n");
